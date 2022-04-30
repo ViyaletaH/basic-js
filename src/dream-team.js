5ec286c5874da6a04094dd42842de4c81a8ae2c1
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-30 19:37:40 +0200

    feat: add partial solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function createDreamTeam(members) {
+  let abbr;
+  let preFinal = [];
+  let final = [];
+  let preStr;
+  if (Array.isArray(members)) {
+    for (i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      members[i] = members[i].trim().slice(0, 1);
+    } 
+    else {
+      let removed = members.splice([i], 1);
+    }
+
+  }
+    let k = 0; 
+    while (k < members.length) {
+      if (members[k].length > 1) {
+        members[k] = members[k].trim().slice(0, 1);
+      }
+      else {
+        k++;
+      }
+    }
+    members = members.sort().join().toUpperCase();;
+    abbr = members.replace(/[,]/gi, '');
+
+  } else {
+    abbr = false;
+  };
+  
+  return abbr;
 }
 
 module.exports = {
